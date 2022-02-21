1b04d38c1e69038692db3e76ab0d923f3b457c0d
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-02-21 12:55:09 -0500

    Extend knight moves.
    Extend knight move generation to include id.

--- lib/modules/moves/knight_move.rb
+++ lib/modules/moves/knight_move.rb
@@ -4,38 +4,37 @@
 # Validates if a knight is moving correctly
 module KnightMove
   def generate_knight_moves(board_state)
-    generate_moves(position, board_state, 2, 1)
-      .concat(generate_moves(position, board_state, 1, 2))
+    generate_moves(position, board_state, [2, 1])
+      .concat(generate_moves(position, board_state, [1, 2]))
   end
 
   private
 
-  def generate_moves(initial, board_state, column_operator, row_operator) # rubocop:disable Metrics/MethodLength
+  def generate_moves(initial, board_state, operators) # rubocop:disable Metrics/MethodLength
     moves = []
-    initial_column, initial_row = initial
 
-    move_combinations(column_operator, row_operator).each do |column_change, row_change|
-      new_position = calculate_next_knight_position(initial_column, initial_row, column_change, row_change)
+    move_combinations(operators).each do |changes|
+      new_position = calculate_next_knight_position(initial, changes)
 
       next unless inside_board?(new_position)
 
       obstacle_piece = board_state.find { |piece| piece.position == new_position }
 
       if obstacle_piece.nil?
-        moves << [new_position, false]
+        moves << PossibleMove.new(new_position, false)
       else
-        moves << [new_position, true] unless obstacle_piece.color == color
+        moves << PossibleMove.new(new_position, true) unless obstacle_piece.color == color
       end
     end
 
     moves
   end
 
-  def move_combinations(column_operator, row_operator)
-    [-column_operator, column_operator].product([-row_operator, row_operator])
+  def move_combinations(operators)
+    [-operators[0], operators[0]].product([-operators[1], operators[1]])
   end
 
-  def calculate_next_knight_position(initial_column, initial_row, column_change, row_change)
-    [(initial_column.ord + column_change).chr, initial_row + row_change]
+  def calculate_next_knight_position(initial, operators)
+    [(initial[0].ord + operators[0]).chr, initial[1] + operators[1]]
   end
 end
